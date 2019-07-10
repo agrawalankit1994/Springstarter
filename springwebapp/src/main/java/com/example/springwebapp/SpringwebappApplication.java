eb98581218cfcc9f87a892a367ebe9daf7c2b52b
Ankit <Ankit@Ankit-PC>
2019-07-10 10:05:54 +0530

    First commit

---
+++ springwebapp/src/main/java/com/example/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,15 @@
+package com.example.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+		System.out.println("Spring Project");
+		System.out.println();
+	}
+}
